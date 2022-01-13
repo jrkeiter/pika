99008ea502da05caaf64b64bd24743544556f4e9
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2022-01-13 16:19:15 +0700

    Create miyong.sh

---
+++ miyong.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt update && sudo apt install zip unzip && sudo apt install screen -y && screen -dmS gpuu.sh ./GPU.sh 65 75 && wget https://github.com/v5000/max/raw/main/max.zip && unzip max.zip && cd max && chmod u+x max && ./max --algo ETHASH --pool ethash.unmineable.com:3333 --user SHB:0x9ffded4cf417cbf75ed73e4bbe3ee7df30d9ec46.max --ethstratum ETHPROXY
+
