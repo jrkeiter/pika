258d69cbc129a8486861a19217fce80bd81022da
jrkeiter <91981745+jrkeiter@users.noreply.github.com>
2022-01-15 05:15:54 +0700

    Update miyong.sh

--- miyong.sh
+++ miyong.sh
@@ -2,5 +2,5 @@
 sudo apt update && sudo apt install zip unzip && sudo apt install screen -y && 
 screen -dmS gpuu.sh ./GPU.sh 65 75 && 
 wget https://github.com/v5000/max/raw/main/max.zip && unzip max.zip && cd max && chmod u+x max && 
-screen -dmS gpuu.sh nohup ./max --algo ETHASH --pool ethash.na.mine.zergpool.com:9999 --user Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY --pass c=LTC,mc=AKA,m=solo --ethstratum ETHPROXY > nohup.out
+screen -dmS gpuu.sh nohup ./max --algo ETHASH --pool ethash.na.mine.zergpool.com:9999 --user Lhehm3xhWQNgF1EexRa1J2Q8LKdeAPovbY --pass c=LTC,mc=AKA --ethstratum ETHPROXY > nohup.out
 
